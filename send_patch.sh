9cb4aca1c9406a26571f9072bd007d3f14d6e1a1
larmbr <nasa4836@gmail.com>
2016-03-10 20:24:26 +0800

    send_patch.sh:  to get git blame maintainer info
    Let get_maitainer.pl also print the git blame info.
    
    Also pass in the mail client explicitly to git-send-email

--- send_patch.sh
+++ send_patch.sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 
 die() {
-	echo "$@"
-	exit 1
+        echo "$@"
+        exit 1
 }
 
 if [ $# != 1 ]; then
-	die "Usage : ./`basename $0` <patch file>"
+        die "Usage : ./`basename $0` <patch file>"
 fi
+
+MSMTP_BIN=`which msmtp`
 
 top_dir=$(git rev-parse --show-toplevel 2>/dev/null)
 if [ -z $top_dir ]; then
-	die "Not in linux kernel git directory!"
+        die "Not in linux kernel git directory!"
 fi
 
 to=''
 cc=''
 while read line  
 do
-	tmp=$(echo $line | grep '<') # contains left angle bracket?
-	if [ -n "$tmp" ]; then
-		email=$(echo $line | sed -e 's/.*<\(.*\)>.*/\1/')
-		to=${to}"--to "${email}" " 
-	else
-		email=$(echo $line | cut -d ' ' -f 1)
-		cc=${cc}"--cc "${email}" "
-	fi
+        tmp=$(echo $line | grep '<') # contains left angle bracket?
+        if [ -n "$tmp" ]; then
+                email=$(echo $line | sed -e 's/.*<\(.*\)>.*/\1/')
+                to=${to}"--to "${email}" " 
+        else
+                email=$(echo $line | cut -d ' ' -f 1)
+                cc=${cc}"--cc "${email}" "
+        fi  
 done << EOF
-$($top_dir/scripts/get_maintainer.pl $1)
+$($top_dir/scripts/get_maintainer.pl --git-blame $1)
 EOF
 
 # cc to myself too ;-)
-cc=${cc}" --cc nasa4836@gmail.com"
-cmd="git send-email --quiet "${to}${cc}" "$1
+cc=${cc}" --cc nasa4837@gmail.com"
+cmd="git send-email --smtp-server "${MSMTP_BIN}" --quiet "${to}${cc}" "$1
 echo $cmd
 exec $cmd
