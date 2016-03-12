69ac1a6c0108071a693744b1ad8c90fcb958fc94
Jianyu Zhan <nasa4836@gmail.com>
2016-03-12 22:47:26 +0800

    Modify send_patch.sh to support sending a patch series.

--- send_patch.sh
+++ send_patch.sh
@@ -5,8 +5,8 @@ die() {
         exit 1
 }
 
-if [ $# != 1 ]; then
-        die "Usage : ./`basename $0` <patch file>"
+if [ $# == 0 ]; then
+        die "Usage : ./`basename $0` <patch file or patches directory>"
 fi
 
 MSMTP_BIN=`which msmtp`
@@ -16,24 +16,53 @@ if [ -z $top_dir ]; then
         die "Not in linux kernel git directory!"
 fi
 
-to=''
-cc=''
-while read line  
-do
+TO_LIST=''
+CC_LIST=''
+
+git_blame_one() {
+    local patch=$1
+    local tmp
+    local email
+
+    echo "Parsing patch: ${patch##/}..."
+
+    while read line  
+    do
         tmp=$(echo $line | grep '<') # contains left angle bracket?
         if [ -n "$tmp" ]; then
-                email=$(echo $line | sed -e 's/.*<\(.*\)>.*/\1/')
-                to=${to}"--to "${email}" " 
+            email=$(echo $line | sed -e 's/.*<\(.*\)>.*/\1/')
+            if [[ "${TO_LIST}" != *"${email}"* ]]; then
+                TO_LIST=${TO_LIST}"--to "${email}" " 
+	    fi
         else
-                email=$(echo $line | cut -d ' ' -f 1)
-                cc=${cc}"--cc "${email}" "
+            email=$(echo $line | cut -d ' ' -f 1)
+            if [[ "${CC_LIST}" != *"${email}"* ]]; then
+                CC_LIST=${CC_LIST}"--cc "${email}" "
+	    fi
         fi  
-done << EOF
-$($top_dir/scripts/get_maintainer.pl --git-blame $1)
+    done << EOF
+$($top_dir/scripts/get_maintainer.pl --git-blame ${patch} 2>/dev/null)
 EOF
+}
+
+if [ -f "$1" ]; then
+	git_blame_one "$1"
+elif [ -d "$1" ]; then
+	for p in `ls ${1%/}/*patch 2>/dev/null`; do
+		git_blame_one "$p"
+	done
+fi
 
 # cc to myself too ;-)
-cc=${cc}" --cc nasa4837@gmail.com"
-cmd="git send-email --smtp-server "${MSMTP_BIN}" --quiet "${to}${cc}" "$1
+FINAL_CC_LIST=${CC_LIST}" --cc nasa4837@gmail.com"
+cmd="git send-email --smtp-server "${MSMTP_BIN}" --confirm=never "${TO_LIST}${FINAL_CC_LIST}" "$1
+
+echo
 echo $cmd
-exec $cmd
+echo
+
+read -s -n 1 -p "Want to send using above command? [Y/N]" answer
+if [ "$answer" == "Y" -o "$answer" == "y" ]; then
+    exec $cmd
+fi
+echo
